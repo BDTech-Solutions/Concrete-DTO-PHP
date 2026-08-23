3069239d143128ebd3d75cc0364aa2c742746b8a
daniel <skyrimdaniel11@gmail.com>
2026-08-23 05:27:47 -0300

    feat: add test to array

--- tests/Unit/DataTransferObjectTest.php
+++ tests/Unit/DataTransferObjectTest.php
@@ -2,16 +2,29 @@
 
 use BdtechSolutions\ConcreteDto\DataTransferObject;
 
+// Declare a class to test dto abstract class
 class UserDTO extends DataTransferObject
 {
   public function __construct(public readonly string $name) {}
 }
 
+beforeEach(function () {
+  // Init class with user name
+  $this->userData = new UserDTO(name: "Daniels 🤗");
+});
+
 it('can init a data transfer object', function () {
-  // init class with user name
-  $userData = new UserDTO(name: "Daniels 🤗");
-  // verify instance type
-  expect($userData)->toBeInstanceOf(DataTransferObject::class);
-  // verify name parameter
-  expect($userData->name)->toBe($userData->name);
+  // Verify instance type
+  expect($this->userData)->toBeInstanceOf(DataTransferObject::class);
+  // Verify name parameter
+  expect($this->userData->name)->toBe($this->userData->name);
+});
+
+it('can convert DTO data to an array', function () {
+  // convert to array
+  $DTOasArray = $this->userData->toArray();
+  // verify if is an array
+  expect($DTOasArray)->toBeArray();
+  // verify data integrity
+  expect($DTOasArray['name'])->toBe('Daniels 🤗');
 });
